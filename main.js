e96a6ba7d78805db2c7691ebd652198d41760077
Djemabdelmalek <59617502+Djemabdelmalek@users.noreply.github.com>
2021-09-30 12:22:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function show(){
+    var element = document.getElementById('ulphone');
+    var element2 = document.getElementById('arrow');
+    var element3 = document.getElementById('littlebox');
+
+    element.classList.toggle('show');
+    element2.classList.toggle('show');
+    element3.classList.toggle('show');
+}
